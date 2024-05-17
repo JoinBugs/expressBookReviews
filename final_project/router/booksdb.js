fb026f625f26d2e1a014ae8c49ad398df6a72cf9
joinbugs <cornejo.cruz.andres@gmail.com>
2024-05-16 20:35:59 -0400

    added auth and general methods

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = [
-      {"isbn": "978-1-60309-502-0", "author": "Chinua Achebe","title": "Things Fall Apart", "reviews": "0-the best book I have read in my life" },
-      {"isbn": "978-1-60309-502-1", "author": "Hans Christian Andersen","title": "Fairy tales", "reviews": "1-the best book I have read in my life" },
-      {"isbn": "978-1-60309-502-2", "author": "Dante Alighieri","title": "The Divine Comedy", "reviews": "2-the best book I have read in my life" },
-      {"isbn": "978-1-60309-502-3", "author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": "3-the best book I have read in my life" },
-      {"isbn": "978-1-60309-502-4", "author": "Unknown","title": "The Book Of Job", "reviews": "4-the best book I have read in my life" },
-      {"isbn": "978-1-60309-502-5", "author": "Unknown","title": "One Thousand and One Nights", "reviews": "5-the best book I have read in my life" },
-      {"isbn": "978-1-60309-502-6", "author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": "6-the best book I have read in my life" },
-      {"isbn": "978-1-60309-502-7", "author": "Jane Austen","title": "Pride and Prejudice", "reviews": "7-the best book I have read in my life" },
-      {"isbn": "978-1-60309-502-8", "author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": "8-the best book I have read in my life" },
-      {"isbn": "978-1-60309-502-9", "author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": "9-the best book I have read in my life" }
+      {"isbn": "978-1-60309-502-0", "author": "Chinua Achebe","title": "Things Fall Apart", "reviews": ["0-the best book I have read in my life"] },
+      {"isbn": "978-1-60309-502-1", "author": "Hans Christian Andersen","title": "Fairy tales", "reviews": ["1-the best book I have read in my life"] },
+      {"isbn": "978-1-60309-502-2", "author": "Dante Alighieri","title": "The Divine Comedy", "reviews": ["2-the best book I have read in my life"] },
+      {"isbn": "978-1-60309-502-3", "author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": ["3-the best book I have read in my life"] },
+      {"isbn": "978-1-60309-502-4", "author": "Unknown","title": "The Book Of Job", "reviews": ["4-the best book I have read in my life"] },
+      {"isbn": "978-1-60309-502-5", "author": "Unknown","title": "One Thousand and One Nights", "reviews": ["5-the best book I have read in my life"] },
+      {"isbn": "978-1-60309-502-6", "author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": ["6-the best book I have read in my life"] },
+      {"isbn": "978-1-60309-502-7", "author": "Jane Austen","title": "Pride and Prejudice", "reviews": ["7-the best book I have read in my life"] },
+      {"isbn": "978-1-60309-502-8", "author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": ["8-the best book I have read in my life"] },
+      {"isbn": "978-1-60309-502-9", "author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": ["9-the best book I have read in my life"] }
 ]
 
 module.exports=books;
